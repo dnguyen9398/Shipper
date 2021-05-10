44d3c35ff269fdac3d843d0c9859d3ec3e04e86d
dnguyen9398 <dunguyen9398@gmail.com>
2021-05-10 13:02:36 +0700

    First commit

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { MAIN_COLOR, WHITE } from './asset/color';
 
 const Shipper = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello world!</Text>
-    </View>
+    <SafeAreaView
+      style={styles.container}>
+      <Image
+      source={require('./img/logo.png')}
+      style={styles.logo}
+      ></Image>
+      <Text
+        style={{top:114, alignSelf: 'center', fontSize: 20, color: WHITE}}>
+        Nhập số điện thoại để tiếp tục
+      </Text>
+    </SafeAreaView>
   )
 }
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: MAIN_COLOR
+  },
+  logo:{
+    alignSelf: 'center',
+    marginTop: 100
+  }
+})
 export default Shipper;
---
+++ asset/color.js
@@ -0,0 +1,2 @@
+export const MAIN_COLOR = '#1A2238'
+export const WHITE = '#FFFFFF'
